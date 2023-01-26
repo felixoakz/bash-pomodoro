50549287d508276047358802ee782f02b160ba45
felixoakz <felix@oak.org>
2023-01-26 18:17:51 -0300

    creating repo

---
+++ pomo.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+# made by @felioakz + google + chatGPT :)
+
+# before executing it, create the binnary by running: % chmod +x pomodoro.sh   
+# to execute: $ ./pomodoro.sh [argument] (argument: an interger representing minutes)
+
+# Check if a duration argument was provided
+if [ -z "$1" ]; then
+  echo "Error: Please provide the duration of the Pomodoro in minutes as an argument."
+  exit 1
+
+# validation with regex to check input as interger
+else
+    # Check if the input is an integer
+    if [[ $1 =~ ^[0-9]+$ ]]; then
+        duration=$1
+    else
+        echo "Error: Please provide a valid integer as the duration of the Pomodoro in minutes."
+        exit 1
+    fi
+fi
+
+# Start the timer
+echo "POMODORO TIMER started for $duration minutes ($(date) GMT)"
+
+# Use a loop to update the status bar every second
+for ((i=1; i<=duration*60; i++)); do
+    # Calculate the percentage of the duration that has passed
+    percentage=$((i*100/(duration*60)))
+    
+    # Print the status bar
+    printf "\r[%-$((duration*60/100*percentage))s] %d%%" "ELAPSED" $percentage
+
+    # print a pipe to each %
+    for ((j=1; j<=percentage; j++)); do
+        printf "|"
+    done
+    
+    # Sleep for one second
+    sleep 1
+done
+# Notify the user that the Pomodoro is finished
+notify-send "Time's up! POMODORO TIMER Completed!"
+
+# Play system alert sound
+paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
